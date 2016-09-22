df0970e53d85a40e9d360a0d6b0327904c18126c
张玉琳 <2489318084@qq.com>
2016-09-22 17:08:47 +0800

    Initial Commit

---
+++ gaoDeCeShi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gaoDeCeShi
+//
+//  Created by 石家庄盛航 on 16/9/22.
+//  Copyright © 2016年 sjzshtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gaoDeCeShi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gaoDeCeShi
+//
+//  Created by 石家庄盛航 on 16/9/22.
+//  Copyright © 2016年 sjzshtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
